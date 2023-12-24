1b5c3a364893c821875a4af82d51fb5fcae9cbc8
Hanif Naufal <rafandinaufal03@gmail.com>
2023-12-24 18:25:19 +0700

    OCR ML

---
+++ lib/constants/route.dart
@@ -0,0 +1,6 @@
+const loginRoute = '/login';
+const registerRoute = '/register';
+const verifyEmailRoute = '/verify-email';
+const bookRoute = '/book';
+const bookListRoute = '/book-list';
+const scanViewRoute = '/scan';
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: '',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const HomePage(),
+      routes: {
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+        scanViewRoute: (context) => const ScanView(),
+        bookRoute: (context) => const BookView(),
+        BookListRoute: (context) => const BookListView(),
+      },
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const BookView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
+
